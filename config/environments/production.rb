573e378fa626f92bce48fe144e6285acba4a3946
DeVuDeveloper <dejanvu.developer@gmail.com>
2023-10-02 04:18:32 +0200

    render deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   config.assets.compile = false
 
