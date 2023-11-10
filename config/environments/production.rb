ddff65b9ad6c31e65dedfd5860ae4f3d5c9dde03
DeVuDeveloper <dejanvu.developer@gmail.com>
2023-11-10 02:09:16 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
 
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  config.public_file_server.enabled = true
+
   config.assets.compile = false
 
   config.active_storage.service = :cloudinary
