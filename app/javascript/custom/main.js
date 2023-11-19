6ffba708cbc97b5761b06caa65b5841f22b04b39
DeVuDeveloper <dejanvu.developer@gmail.com>
2023-11-19 08:38:39 +0100

    fix g analytics

--- app/javascript/custom/main.js
+++ app/javascript/custom/main.js
@@ -25,22 +25,6 @@ class CookieBar {
         this.cookiesBar.querySelector('.reject').addEventListener('click', () => this.allowCookies(false));
     }
 
-    appendGACode() {
-        const gaScript = document.createElement('script');
-        gaScript.async = true;
-        gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-081RNEE4JJ';
-        document.head.appendChild(gaScript);
-    
-        gaScript.onload = () => {
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-                window.dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-            gtag('config', 'G-081RNEE4JJ');
-        };
-    }
-
     allowCookies(allow) {
         if (allow) {
             Cookies.set('allow_cookies', 'yes', { expires: 365 });
